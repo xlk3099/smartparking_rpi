76af50399c568e7dad08c8e7edf54867cdce3c95
xlk3099 <xlk3099@gmail.com>
2016-11-29 14:55:15 +0800

    Error fix

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ output_chan_list = [22, 24, 26]
 GPIO.setup(output_chan_list, GPIO.OUT, initial=GPIO.HIGH)
 
 while True:
-        # Update the status of LED
-        if GPIO.input(8) == 0:
-            GPIO.output(22, GPIO.LOW)
+    for index, _ in enumerate(input_chan_list):
+        if GPIO.input(chan) == 0:
+            GPIO.output(output_chan_list[index], GPIO.LOW)
         else:
-            GPIO.output(, GPIO.HIGH)
+            GPIO.output(output_chan_list[index], GPIO.HIGH)
         # update the status of car slots available or not 
         data[index+1]["available"] = True if GPIO.input(chan) == 1 else False
